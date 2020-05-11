e192e48dd251a694606767bdb8a463db2f5965c4
codedev85 <zaccheausolawuyi@gmail.com>
2020-05-11 02:26:42 -0500

    autocomplete off

--- resources/views/Doctor/show.blade.php
+++ resources/views/Doctor/show.blade.php
@@ -32,10 +32,10 @@
                     </div>
                     <div class="col-md-7 mt-3">
                         <h6><b>Address: </b>{{ $findPatient->address }}</h6>
-                        @if(!$findPatient->state->isEmpty())
+                        {{-- @if(!$findPatient->state->isEmpty())
                         <h6><b>State: </b>{{ Ucfirst($findPatient->state['name']) }} State</h6>
                         <h6><b>LGA: </b>{{ $findPatient->lga['name'] }}</h6>
-                        @endif
+                        @endif --}}
                     </div>
                 </div>
             </div>
